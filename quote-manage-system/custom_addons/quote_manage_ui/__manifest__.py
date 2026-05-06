f2d450f8b7e51625e48b08c21d7d720529459481
chrischan <ziqichen55555@gmail.com>
2026-05-06 16:12:11 +0800

    Update Codespaces configuration for improved performance and compatibility

---
+++ quote-manage-system/custom_addons/quote_manage_ui/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': '报价管理系统 后台 UI 改善',
+    'version': '1.0',
+    'category': 'Hidden',
+    'summary': 'Improve the Backend UI of the quote management system',
+    'depends': ['web'],
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            'quote_manage_ui/static/src/scss/backend_style.scss',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
