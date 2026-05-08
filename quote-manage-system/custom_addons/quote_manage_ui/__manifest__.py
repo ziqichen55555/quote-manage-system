216f98447acc93c9c540bb3fb137c2f751ddf83a
chrischan <ziqichen55555@gmail.com>
2026-05-08 13:38:03 +0800

    Integrate Nginx as a reverse proxy for Odoo and enable proxy mode in configuration. Update quote management UI dependencies and enhance frontend styles with new SCSS variables. Improve backend styles for better visual consistency, including navbar and button adjustments.

--- quote-manage-system/custom_addons/quote_manage_ui/__manifest__.py
+++ quote-manage-system/custom_addons/quote_manage_ui/__manifest__.py
@@ -3,16 +3,14 @@
     'version': '1.0',
     'category': 'Hidden',
     'summary': 'Improve the Backend UI of the quote management system',
-    'depends': ['web', 'sale'],
+    'depends': ['web', 'sale', 'website', 'website_sale', 'website_blog', 'website_crm'],
     'data': [
         'views/sale_order_views.xml',
+        'views/website_templates.xml',
     ],
     'assets': {
         'web.assets_backend': [
             'quote_manage_ui/static/src/scss/backend_style.scss',
-        ],
-        'web.assets_frontend': [
-            'quote_manage_ui/static/src/scss/style.scss',
         ],
     },
     'installable': True,
