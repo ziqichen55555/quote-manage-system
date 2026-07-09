700b36d924c43ab42a38f905017fd08e96fc3be3
chris <intern@cocreativeit.com>
2026-07-09 16:23:19 +0800

    Update version to 17.0.1.0.9, add eBay payment method integration with automatic line creation for associated journals during installation and upgrades.

--- quote-manage-system/custom_addons/account_xero/models/account_payment_method.py
+++ quote-manage-system/custom_addons/account_xero/models/account_payment_method.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models
+from odoo import api, models
 
 
 class AccountPaymentMethod(models.Model):
@@ -13,3 +13,44 @@ class AccountPaymentMethod(models.Model):
             'domain': [('type', 'in', ('bank', 'cash'))],
         }
         return info
+
+    def _register_hook(self):
+        """Ensure eBay method + lines exist on install and on upgrade/restart."""
+        res = super()._register_hook()
+        self._ensure_ebay_method_lines()
+        return res
+
+    @api.model
+    def _ensure_ebay_method_lines(self):
+        method = self.sudo().search([
+            ('code', '=', 'ebay'),
+            ('payment_type', '=', 'inbound'),
+        ], limit=1)
+        if not method:
+            method = self.sudo().create({
+                'name': 'eBay',
+                'code': 'ebay',
+                'payment_type': 'inbound',
+            })
+
+        journals = self.env['account.journal'].sudo().search([
+            ('type', 'in', ('bank', 'cash')),
+        ])
+        if not journals:
+            return
+
+        line_model = self.env['account.payment.method.line'].sudo()
+        existing_lines = line_model.search([
+            ('payment_method_id', '=', method.id),
+            ('journal_id', 'in', journals.ids),
+        ])
+        existing_journal_ids = set(existing_lines.mapped('journal_id').ids)
+        missing_journals = journals.filtered(lambda j: j.id not in existing_journal_ids)
+        if not missing_journals:
+            return
+
+        line_model.create([{
+            'name': method.name,
+            'payment_method_id': method.id,
+            'journal_id': journal.id,
+        } for journal in missing_journals])
